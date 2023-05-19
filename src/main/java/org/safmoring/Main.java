d69017aeddbb64e2b9631b22574214ff43f8852f
verah makori <verahmakori@gmail.com>
2023-05-19 13:45:09 +0300

    updated the main function

--- src/main/java/org/safmoring/Main.java
+++ src/main/java/org/safmoring/Main.java
@@ -1,19 +1,41 @@
 package org.safmoring;
+import org.safmoring.dao.Hero_Dao;
+import org.safmoring.dao.Squad_Dao;
+import org.safmoring.model.Hero;
+import org.safmoring.model.Squad;
+import org.safmoring.DB.db_connection;
 
+import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFiles;
+
 public class Main {
-    staticFiles.location("/public");
 
-    // Set up the Handlebars template engine
-    HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
+    public static void main(String[] args) {
+        staticFiles.location("/public");
 
-    // Set up the database connector
-    DatabaseConnector databaseConnector = new DatabaseConnector("jdbc:postgresql://localhost:5432/superhero_recruitment", "postgres", "Nyangau92");
+        // Set up the Handlebars template engine
+        HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
+
+        // Set up the database connector
+        DatabaseConnector databaseConnector = new DatabaseConnector("jdbc:postgresql://localhost:5432/superhero_recruitment", "postgres", "Nyangau92");
         databaseConnector.init();
 
-    // Set up the HeroDao and SquadDao
-    HeroDao heroDao = new HeroDao(databaseConnector.getSql2o());
-    SquadDao squadDao = new SquadDao(databaseConnector.getSql2o());
+        // Set up the HeroDao and SquadDao
+        HeroDao heroDao = new HeroDao(databaseConnector.getSql2o());
+        SquadDao squadDao = new SquadDao(databaseConnector.getSql2o());
+
+        // Home page route
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("heroes", heroDao.getAllHeroes());
+            model.put("squads", squadDao.getAllSquads());
+            return new ModelAndView(model, "index.hbs");
+        }, templateEngine);
     }
 }
--- src/main/java/org/safmoring/dao/Hero_Dao.java
+++ src/main/java/org/safmoring/dao/Hero_Dao.java
@@ -1,8 +1,16 @@
 package org.safmoring.dao;
 
 import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
+import org.safmoring.model.Hero;
 public class Hero_Dao {
+    private final Sql2o sql2o;
+
+    public Hero_Dao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
     public void addHero(Hero hero) {
         try (Connection connection = sql2o.open()) {
             String query = "INSERT INTO heroes (name, age, power, weakness) VALUES (:name, :age, :power, :weakness)";
