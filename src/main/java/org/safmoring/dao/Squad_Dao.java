d70bbbbfe1b5f4fb30d512d80f1230c89ff654cc
verah makori <verahmakori@gmail.com>
2023-05-19 14:32:57 +0300

    added get all squads function

--- src/main/java/org/safmoring/dao/Squad_Dao.java
+++ src/main/java/org/safmoring/dao/Squad_Dao.java
@@ -4,6 +4,8 @@ import org.safmoring.model.Squad;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.List;
+
 public class Squad_Dao {
     private final Sql2o sql2o;
 
@@ -19,6 +21,14 @@ public class Squad_Dao {
                     .addParameter("maxSize", squad.getMaxSize())
                     .addParameter("cause", squad.getCause())
                     .executeUpdate();
+        }
+    }
+
+    public List<Squad> getAllSquads() {
+        String sql = "SELECT * FROM squads";
+        try (Connection con = sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(Squad.class);
         }
     }
 }
